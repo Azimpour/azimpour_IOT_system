1812b707200aed77e18f309658cbd30519c6a86d
banafshe <banafshe.a70@gmail.com>
2025-02-07 17:21:41 +0330

    Create iotproject.py

---
+++ iotproject.py
@@ -0,0 +1,48 @@
+class Device():
+    
+    def __init__(self,topic):
+
+        self.topic=topic
+        self.topic_list=topic.split('/')
+        self.location=self.topic_list[0]
+        self.group=self.topic_list[1]
+        self.device_type=self.topic_list[2]
+        self.name=self.topic_list[3]
+        self.status='off'
+
+
+    def turn_on(self):
+        
+        self.status='on'
+        if name_device in topic:
+            if st=='turn on':
+                print(f'{name_device} is turned on')
+        
+
+    def turn_off(self):
+
+        self.status='off'
+        if name_device in topic:
+            if st=='turn off':
+                print(f'{name_device} is turned off')
+        
+
+    def get_status(self):
+        return self.status
+    
+
+
+topic=input('please enter (location/group/device_type/name):')
+dev=Device(topic)
+print(f' {topic} is created')
+
+
+name_device=input('please enter your device name:')
+st=input('turn off or turn on?')
+
+if st=='turn on':
+   topic.turn_on()
+elif st=='turn off':
+    topic.turn_off()
+
+st.get_status()
